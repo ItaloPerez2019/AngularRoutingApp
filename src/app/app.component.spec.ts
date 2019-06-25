d128a9b6e58f3359244c5d23aea888e525a84f6f
italo <iep4.njit.edu>
2019-06-25 19:00:48 -0400

    RoutingApp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sampleAngular'`, () => {
+  it(`should have as title 'routing-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('sampleAngular');
+    expect(app.title).toEqual('routing-demo');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sampleAngular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to routing-demo!');
   });
 });
